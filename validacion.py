7f54c879824997ef767372352b4484ad3f30c177
Elio6174 <elseismylife@gmail.com>
2026-04-19 02:09:27 -0600

    modelos entrenados

--- validacion.py
+++ validacion.py
@@ -4,11 +4,12 @@ import numpy as np
 from tensorflow.keras.utils import load_img,img_to_array
 from keras.models import load_model
 import os.path
+from pathlib import Path
 
 #Leer la imagen a evaluar
 
-#imagen = "archive/train/cats/cat_395.jpg"
-imagen = "archive/train/dogs/dog_43.jpg"
+#imagen = "archive/train/cats/cat_2.jpg"
+#imagen = "archive/train/dogs/dog_0.jpg"
 
 #Recortar la imagen
 
@@ -21,18 +22,20 @@ modelo = "modelo_CatDog.keras"
 cnn = load_model(modelo)
 
 
-#Transformar la imagen a clasificar
-imagen_clasificar = load_img(imagen,target_size=(altura,anchura))
-imagen_clasificar = img_to_array(imagen_clasificar)
-imagen_clasificar =imagen_clasificar / 255.0
-imagen_clasificar = np.expand_dims(imagen_clasificar,axis =0)
+for imagen in Path('archive/train/dogs/').iterdir():
+  #Transformar la imagen a clasificar
+  imagen_clasificar = load_img(imagen,target_size=(altura,anchura))
+  imagen_clasificar = img_to_array(imagen_clasificar)
+  imagen_clasificar =imagen_clasificar / 255.0
+  imagen_clasificar = np.expand_dims(imagen_clasificar,axis =0)
 
-#Evaluar la imagen
-prediccion = cnn.predict(imagen_clasificar)
-print(f"Probabilidades por clase: {prediccion}")
-arg_max = np.argmax(prediccion)
+  #Evaluar la imagen
+  prediccion = cnn.predict(imagen_clasificar)
+  print(f"Probabilidades por clase: {prediccion}")
+  arg_max = np.argmax(prediccion)
+  print(f"argMax:{arg_max}")
 
-if arg_max == 0:
-  print("Gato")
-elif(arg_max==1):
-  print("Perro")
+  if arg_max == 0:
+    print("Gato")
+  elif(arg_max==1):
+    print("Perro")
